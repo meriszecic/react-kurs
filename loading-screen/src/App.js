86b2f207225d410241ea5d5ae5deff39faca3cdd
Meris <meriszecic@gmail.com>
2022-07-18 17:09:48 +0200

    Loading-screen create

---
+++ loading-screen/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      Loading
+      <div className='spiner spiner-red'/>
+      <div className='spiner spiner-blue'/>
+      <div className='spiner spiner-green'/>
+    </div>
+  );
+}
+
+export default App;
